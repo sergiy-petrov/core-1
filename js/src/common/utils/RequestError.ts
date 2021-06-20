5165eaceadf6abc4f75ad1ad8e68825a2ac28e55
David Sevilla Martin <me@datitisev.me>
2021-06-20 11:37:26 -0400

    Update common/states/AlertManagerState & common/utils/RequestError

--- js/src/common/utils/RequestError.ts
+++ js/src/common/utils/RequestError.ts
@@ -1,21 +1,21 @@
 export default class RequestError {
-  status: string;
+  status: string | number;
   options: object;
   xhr: XMLHttpRequest;
 
   responseText: string | null;
-  response: object | null;
+  response: any | null;
 
   alert: any;
 
-  constructor(status: string, responseText: string | null, options: object, xhr: XMLHttpRequest) {
+  constructor(status: string | number, responseText: string | null, options: object, xhr: XMLHttpRequest) {
     this.status = status;
     this.responseText = responseText;
     this.options = options;
     this.xhr = xhr;
 
     try {
-      this.response = JSON.parse(responseText);
+      this.response = JSON.parse(responseText!);
     } catch (e) {
       this.response = null;
     }
